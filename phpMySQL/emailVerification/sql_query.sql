20062bf5d86751bfb191aabfec0e0de585c3d098
empsloc <empsloc@gmail.com>
2024-04-24 12:47:40 +0530

    sdsd

--- phpMySQL/emailVerification/sql_query.sql
+++
@@ -1,8 +0,0 @@
-CREATE database email;
-
-USE email;
-
-CREATE TABLE registered_emails (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE
-);
--- phpMySQL/groceryStoreApplication/sql_query.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE grocery;
-USE grocery;
-CREATE TABLE items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    quantity INT NOT NULL
-);
